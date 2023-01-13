d1fdc6d8ed8daf030bf73a898a0b281cc148ae86
draco <122390064+Darco786@users.noreply.github.com>
2023-01-14 02:08:00 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,27 @@
 import Button from "@/components/UI/Button";
 import Navbar from "@/components/UI/Navbar";
-import { useCallback } from "react";
-import Particles from "react-particles";
-import { loadFull } from "tsparticles";
-import Nasa from './particles.json'
+// import { useCallback } from "react";
+// import Particles from "react-particles";
+// import { loadFull } from "tsparticles";
+// import Nasa from './particles.json'
 
 
-  const particlesInit = useCallback(async engine => {
-      console.log(engine);
-      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-      // starting from v2 you can add only the features you need reducing the bundle size
-      await loadFull(engine);
-  }, []);
+  // const particlesInit = useCallback(async engine => {
+  //     console.log(engine);
+  //     // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+  //     // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+  //     // starting from v2 you can add only the features you need reducing the bundle size
+  //     await loadFull(engine);
+  // }, []);
 
-  const particlesLoaded = useCallback(async container => {
-      await console.log(container);
-  }, []);
+  // const particlesLoaded = useCallback(async container => {
+  //     await console.log(container);
+  // }, []);
 
 export default function Home() {
   return (
     <div className="min-h-screen z-10 flex items-end pb-16">
-      <Particles id="tsparticles" url={Nasa} init={particlesInit} loaded={particlesLoaded} />
+      {/* <Particles id="tsparticles" url={Nasa} init={particlesInit} loaded={particlesLoaded} /> */}
       <Navbar />
 <div>
 <img
