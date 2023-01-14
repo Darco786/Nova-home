2bf0a0c5e40549c5b08e5b4812f4802bfd5843f5
draco <122390064+Darco786@users.noreply.github.com>
2023-01-14 22:39:03 +0530

    new

--- components/UI/Navbar.js
+++ components/UI/Navbar.js
@@ -5,11 +5,11 @@ function Navbar() {
   return (
     <div className="absolute top-0 left-0 w-full z-[10000]">
       <div className="container h-[70px] lg:h-[90px] flex items-center justify-between">
-        {/* <img src="images/logo-main.png" className="h-[40px]" alt="" /> */}
-        <h1 className="text-2xl lg:text-3xl font-bold font-myriad logo-txt">NOVA</h1>
+        <img src="images/nova.png" className="h-[80px]" alt="" />
+        {/* <h1 className="text-2xl lg:text-3xl font-bold font-myriad logo-txt">NOVA</h1> */}
 
         <div className="flex items-center space-x-4">
-          <Button>Trade</Button>
+          <Button className='head-btn'>Trade</Button>
         </div>
       </div>
     </div>
